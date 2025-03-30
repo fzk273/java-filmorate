7e93ee91038b7c950b5c490f47be4f5b97fe0a09
fzk <fzk@fzk.fzk>
2025-03-30 22:47:56 +0200

    fix linting

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final static Logger log = LoggerFactory.getLogger(UserController.class);
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final HashMap<Long, User> users = new HashMap<>();
 
     @GetMapping
